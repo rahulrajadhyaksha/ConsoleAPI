928bc882f4adc99506375b623fae6815c15a5311
Rahul Rajadhyaksha <rajadhyaksha.rahul@rtcs-india.in>
2022-03-07 12:27:40 +0530

    Add project files.

---
+++ ConsoleAPI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Confluent.Kafka;
+namespace ConsoleAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "gid-consumers",
+                BootstrapServers = "localhost:9092"
+            };
+
+            using (var consumer=new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("temp-topic");
+                while(true)
+                {
+                    var cr = consumer.Consume();
+                    Console.WriteLine(cr.Message.Value);
+                }
+            }
+        }
+    }
+}
